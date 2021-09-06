2a329a568f97ded776b2652b2c9d1f523044bb61
roy2995 <63130325+roy2995@users.noreply.github.com>
2021-09-05 19:16:51 -0700

    enemy script terminado ajustado para que funcione con un solo script

--- 3_pilares/Assets/Scripts/Enemys/Enemy_Controller_2d.cs
+++ 3_pilares/Assets/Scripts/Enemys/Enemy_Controller_2d.cs
@@ -23,6 +23,7 @@ public class Enemy_Controller_2d : MonoBehaviour
     [Header("Enemy_Detection Settings")]
     [Range(0, 20)] public float AgroRange;
     [Range(0, 10)] public float detecMoveSpeed;
+    public bool detection = false;
     public float x;
     public float y;
 
@@ -33,8 +34,62 @@ public class Enemy_Controller_2d : MonoBehaviour
         Enemy_rig = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        Debug.Log(detection);
+        float distance = Vector2.Distance(transform.position, Player.position);
+        if (detection == false)
+        {
+            Enemy_Patrol();
+        }
+        
+        if (distance < AgroRange)
+        {
+            detection = true;
+            Enemy_Detection();
+        }
+        else
+        {
+            detection = false;
+        }
+    }
 
+    public void Enemy_Detection()
+    {
+        float distances = Vector2.Distance(transform.position, Player.position);
+        if (distances < AgroRange)
+        {
+            //chase player & stop patrol
+            chase_player();  
+        }
+        else
+        {
+            //stop chase & continue patrol
+            detection = false;
+            stopchase();
+        }
+    }
 
+    private void chase_player()
+    {
+        if(transform.position.x < Player.position.x)
+        {
+            Enemy_rig.velocity = new Vector2(detecMoveSpeed, 0);
+            transform.localScale = new Vector2(x, y);
+        }
+        else if (transform.position.x > Player.position.x)
+        {
+            Enemy_rig.velocity = new Vector2(-detecMoveSpeed, 0);
+            transform.localScale = new Vector2(-x, y);
+        }
+    }
+
+    private void stopchase()
+    {
+        //Enemy_rig.velocity = new Vector2(0, 0);
+        Debug.Log("stopchase detectado");
+        detection = false;
+    }
 
 
     public void Enemy_Patrol()
@@ -46,9 +101,20 @@ public class Enemy_Controller_2d : MonoBehaviour
         }
 
         Enemy_rig.velocity = new Vector2(vx, Enemy_rig.velocity.y);
+        if(HitWall() || NearEdge())
+        {
+            if(pat_faceDirection == pat_Left)
+            {
+                changeDirection(pat_Right);
+            }
+            else if(pat_faceDirection == pat_Right)
+            {
+                changeDirection(pat_Left);
+            }
+        }
 
     }
-
+    //patrol funcions
     private bool HitWall()
     {
         bool val = false;
@@ -75,17 +141,40 @@ public class Enemy_Controller_2d : MonoBehaviour
         return val;
     }
 
-    /*private bool NearEdge()
+    private bool NearEdge()
     {
+        bool val = true;
 
-    }*/
+        float castDist = base_cast_dist;
+        Vector3 targetpos = Cast_post.position;
+        targetpos.y -= castDist;
 
+        Debug.DrawLine(Cast_post.position, targetpos, Color.green);
 
+        if (Physics2D.Linecast(Cast_post.position, targetpos, 1 << LayerMask.NameToLayer("Terrain")))
+        {
+            val = false;
+        }
+        else
+        {
+            val = true;
+        }
+        return val;
+    }
 
+    private void changeDirection(string newDirection)
+    {
+        Vector3 newScale = Base_Scale;
 
-
-
-
-
-
+        if(newDirection == pat_Left)
+        {
+            newScale.x = -Base_Scale.x;
+        }
+        else if(newDirection == pat_Left)
+        {
+            newScale.x = Base_Scale.x;
+        }
+        transform.localScale = newScale;
+        pat_faceDirection = newDirection;
+    }
 }
---
+++ 3_pilares/Assets/Scripts/Enemys/Enemy_healt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_healt : MonoBehaviour
+{
+    public int healt = 1;
+    int currentHealt;
+
+    private void Start()
+    {
+        currentHealt = healt;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealt -= damage;
+        Debug.Log("funciona");
+
+        if (currentHealt <= 0)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        Destroy(this.gameObject);
+    }
+}
