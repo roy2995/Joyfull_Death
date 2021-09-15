7fed46aa542373b914723cb2315e6f6e7d1bb264
roy2995 <63130325+roy2995@users.noreply.github.com>
2021-09-15 16:55:26 -0700

    fixed

--- 3_pilares/Assets/Scripts/transition/levelLoader.cs
+++ 3_pilares/Assets/Scripts/transition/levelLoader.cs
@@ -8,21 +8,20 @@ public class levelLoader : MonoBehaviour
     [Header("Loader Scene")]
     public Animator transition;
     public float transitiontime = 1;
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            LoadNextLevel();
-        }
-    }
 
-    void LoadNextLevel()
+    public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        LoadNextLevel();
+    }
+
     IEnumerator LoadLevel(int index)
     {
+        Debug.Log("entra a la corrutina");
         transition.SetTrigger("start");
         yield return new WaitForSeconds(transitiontime);
         SceneManager.LoadScene(index);
