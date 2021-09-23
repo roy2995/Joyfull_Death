1841f02c5e5e51b364d1c53ae7de613ca19c0d1a
DESKTOP-ITBNDSI\Blue <63270991+Bluedragon8300@users.noreply.github.com>
2021-09-23 02:35:04 -0500

    Scripts, NPC, Navmesh
    Se agrego a la escena de pruebas:
    - Testing de ruta para que el npc pueda sentarse (aun sigue en experimentacion ya que tiene algunas fallas).
    - Canvas para el NPC
    - Pop up aids
    - Script de pop up, punto destino.
    - Assets al mapa.

---
+++ Celiaca/Celiaca_Game/Assets/Scripts/ClienteOrden.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClienteOrden : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+}
