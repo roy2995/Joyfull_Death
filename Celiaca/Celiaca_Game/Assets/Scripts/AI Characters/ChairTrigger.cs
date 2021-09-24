6041be1e0f9c7e1c58cd02e24b6d1838fc3cc134
DESKTOP-ITBNDSI\Blue <63270991+Bluedragon8300@users.noreply.github.com>
2021-09-24 03:03:26 -0500

    Rehaciendo lo anterior
    Recuperando los scripts y algunos elementos que se hicieron en el proyecto anterior

---
+++ Celiaca/Celiaca_Game/Assets/Scripts/AI Characters/ChairTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairTrigger : MonoBehaviour
+{
+
+    public GameObject PopUpNoty;
+
+    void Start()
+    {
+        PopUpNoty.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider npc)
+    {
+        if(npc.tag == "AIbot")
+        {
+            Debug.Log("Is Sitting on the chair");
+            StartCoroutine("PopupTimerDelay");
+        }
+    }
+
+    IEnumerator PopupTimerDelay()
+    {
+        PopUpNoty.SetActive(false);
+
+        yield return new WaitForSeconds(10f);
+        PopUpNoty.SetActive(true);
+    }
+}
---
+++ Celiaca/Celiaca_Game/Assets/Scripts/AI Characters/MoveTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform Destination;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(Destination.position);
+    }
+
+}
