cfb69d1e6e6f36f32e93a4de91c47cbf3efc6c43
roy2995 <63130325+roy2995@users.noreply.github.com>
2021-09-24 01:17:38 -0700

    extras arreglados

---
+++ Celiaca/Celiaca_Game/Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public CharacterController controller;
+    public float Speed = 100f;
+    public float turnSmoothTime = .1f;
+    private float turnsmoothvelocity;
+   
+    private void Update()
+    {
+        Move_Player();
+    }
+
+    private void Move_Player()
+    {
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(Vertical, 0f, Horizontal*-1).normalized;
+        if (direction.magnitude >= .1f)
+        {
+            float tragetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, tragetAngle, ref turnsmoothvelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(direction * Speed * Time.deltaTime);
+        }
+
+    }
+}
