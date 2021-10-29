1bb9675cc2f4dbad6bedb6d56d1df320fbc330d4
DESKTOP-ITBNDSI\Blue <63270991+Bluedragon8300@users.noreply.github.com>
2021-10-28 22:38:36 -0500

    Update

--- Draw Game/Flag Landers(construc edition)/Flag Landers/Assets/Scripts/David Scripts/DrawLineExample.cs
+++ Draw Game/Flag Landers(construc edition)/Flag Landers/Assets/Scripts/David Scripts/DrawLineExample.cs
@@ -1,13 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 using UnityEngine.EventSystems;
-    
-public class DrawLineScript : MonoBehaviour, IPointerDownHandler,  IPointerUpHandler
+
+public class DrawLineExample : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public GameObject Panel;
+    public Rigidbody KinematicFlag;
 
     bool _setPanel = false;
 
@@ -25,13 +25,11 @@ public class DrawLineScript : MonoBehaviour, IPointerDownHandler,  IPointerUpHan
     float mZCoord;
 
     [SerializeField]
-    Camera cam; 
+    Camera cam;
 
     [SerializeField]
     Transform Collider_Prefab;
     Transform LastInstantiate_Collider;
-    public float Z_changer;
-    
 
     public void OnPointerDown(PointerEventData eventData)
     {
@@ -57,48 +55,41 @@ public class DrawLineScript : MonoBehaviour, IPointerDownHandler,  IPointerUpHan
     void Start()
     {
         LineGo = new GameObject();
-        LineGo.layer = 9;
+        //LineGo.AddComponent<Rigidbody>();
+        //KinematicFlag = GetComponent<Rigidbody>();
+        //KinematicFlag.isKinematic = true;
+        
         Panel.SetActive(true);
-    }
-
-    void update()
-    {
-        /*if (Input.GetKeyDown(KeyCode.P))
-        {
-            Panel.SetActive(false);
-            _setPanel = false;
-            Debug.Log("Player pressed P");
-        }*/
     }
 
     void FixedUpdate()
     {
-        if (startDrawing /*DrawModeOff.CanDraw*/)
+        if (startDrawing)
         {
-            Debug.Log("Dibuja");
             Vector3 Dist = MousePos - Input.mousePosition;
             float Distance_SqrMag = Dist.sqrMagnitude;
 
-            if(Dist.sqrMagnitude > 1000f)
+            if (Dist.sqrMagnitude > 1000f)
             {
                 _LR.SetPosition(CurrentIndex, cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 10f)));
 
-                if(LastInstantiate_Collider != null)
+                if (LastInstantiate_Collider != null)
                 {
                     Vector3 CurrLinePos = _LR.GetPosition(CurrentIndex);
                     LastInstantiate_Collider.gameObject.SetActive(true);
                     LastInstantiate_Collider.LookAt(CurrLinePos);
 
-                    if(LastInstantiate_Collider.rotation.y == 0)
+                    if (LastInstantiate_Collider.rotation.y == 0)
                     {
                         LastInstantiate_Collider.eulerAngles = new Vector3(LastInstantiate_Collider.rotation.eulerAngles.x, 90, LastInstantiate_Collider.rotation.eulerAngles.z);
                     }
 
-                    LastInstantiate_Collider.localScale = new Vector3(LastInstantiate_Collider.localScale.x, LastInstantiate_Collider.localScale.y, 
-                        Vector3.Distance(LastInstantiate_Collider.position, CurrLinePos) * Z_changer);
+                    LastInstantiate_Collider.localScale = new Vector3(LastInstantiate_Collider.localScale.x, LastInstantiate_Collider.localScale.y,
+                        Vector3.Distance(LastInstantiate_Collider.position, CurrLinePos) * 50f);
                 }
 
                 LastInstantiate_Collider = Instantiate(Collider_Prefab, _LR.GetPosition(CurrentIndex), Quaternion.identity, LineGo.transform);
+
                 LastInstantiate_Collider.gameObject.SetActive(false);
 
                 MousePos = Input.mousePosition;
