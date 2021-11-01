75eb7f01622046292c27c3a97a44dfe99b92418e
roy2995 <63130325+roy2995@users.noreply.github.com>
2021-11-01 07:59:50 -0700

    Delete Intro.avi.meta

---
+++ Draw Game/Flag Landers(construc edition)/Flag Landers/Assets/Scripts/Roy/ultimos/Score.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [Header("Score Settings")]
+    public int end_score;
+    public GameObject score_system;
+    public List<Collider> ballcount = new List<Collider>();
+    public Text score;
+    private bool time_of = false;
+    private bool end = false;
+    private float current_time = 10;
+    private void Awake()
+    {
+        score_system.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (time_of)
+        {
+            StartCoroutine(time_off());
+        }
+
+        if (end || ballcount.Count == 5)
+        {
+            StopCoroutine(time_off());
+            current_time = 0f;
+            score_system.SetActive(true);
+            end_score = ballcount.Count * 500;
+            score.text = end_score.ToString();
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("tagert_score"))
+        {
+            ballcount.Add(other);
+            if (!time_of)
+            {
+                time_of = true;
+            }
+        }
+    }
+
+    IEnumerator time_off()
+    {
+        if(current_time > 0 && !end)
+        {
+            current_time -= Time.deltaTime;
+            Debug.Log(current_time);
+        }
+
+        if (current_time <= 0 || end)
+        {
+            end = true;
+            time_of = false;
+        }
+
+            yield return new WaitForSeconds(1f);
+    }
+}
---
+++ Draw Game/Flag Landers(construc edition)/Flag Landers/Assets/Scripts/Roy/ultimos/ballspawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballspawn : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    private int max_balls = 5;
+    private int coun_balls;
+    [SerializeField] private GameObject ball;
+    [SerializeField] private GameObject spawner;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && coun_balls <=4 )
+        {
+            StartCoroutine(spawn());
+        }
+        else
+        {
+            StopCoroutine(spawn());
+        }
+
+
+    }
+
+    IEnumerator spawn()
+    {
+        Instantiate(ball, spawner.transform.position, Quaternion.identity);
+        coun_balls += 1;
+        yield return new WaitForSecondsRealtime(1f);
+    }
+
+
+
+
+
+
+
+}
