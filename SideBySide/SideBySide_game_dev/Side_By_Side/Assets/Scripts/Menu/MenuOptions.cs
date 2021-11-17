09a06745551f9f111476322697c0a12260827f7d
DESKTOP-ITBNDSI\Blue <63270991+Bluedragon8300@users.noreply.github.com>
2021-11-17 13:28:53 -0500

    Update
    Main menu y opciones añadidos
    - Tienes acceso a hacer quit
    - Puedes usar opciones y apagarlo
    - Puedes cambiar de escena con el nombre de la escena.
    
    Mas updates mas adelante

---
+++ SideBySide/SideBySide_game_dev/Side_By_Side/Assets/Scripts/Menu/MenuOptions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOptions : MonoBehaviour
+{
+
+    public GameObject Options_Panel;
+    public GameObject Options_TurnOn;
+    public GameObject Options_TurnOff;
+
+    void Start()
+    {
+        Options_Panel.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OptionsOn()
+    {
+        Options_Panel.SetActive(true);
+    }
+
+    public void OptionsOff()
+    {
+        Options_Panel.SetActive(false);
+    }
+}
---
+++ SideBySide/SideBySide_game_dev/Side_By_Side/Assets/Scripts/Menu/siguientelvl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class siguientelvl : MonoBehaviour
+{
+    public void next(string sceneToLoade)
+    {
+        SceneManager.LoadScene(sceneToLoade);
+    }
+
+    public void QuitScene()
+    {
+        Application.Quit();
+        Debug.Log("Player has quit");
+    }
+
+    public void resetCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
