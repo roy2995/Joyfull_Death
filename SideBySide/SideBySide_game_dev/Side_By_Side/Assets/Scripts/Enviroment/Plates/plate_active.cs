75f2b7f55091e230d8cbc1f02ab7090ef8b7f23a
roy2995 <63130325+roy2995@users.noreply.github.com>
2021-11-28 14:10:12 -0800

    la placa ya funciona solo debes hacerla prefab

---
+++ SideBySide/SideBySide_game_dev/Side_By_Side/Assets/Scripts/Enviroment/Plates/plate_active.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plate_active : MonoBehaviour
+{
+    [Header("Plate Settings")]
+    public Animator animator;
+    public static bool active_door = false;
+
+
+
+
+
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Box") || collision.CompareTag("Player"))
+        {
+            animator.SetBool("Active", true);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Box") || collision.CompareTag("Player"))
+        {
+            animator.SetBool("Active", true);
+            active_door = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("Active", false);
+    }
+}
